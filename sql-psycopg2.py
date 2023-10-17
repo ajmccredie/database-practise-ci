413d53e7f5e3f483434970a204b3efae51592c8d
ajmccredie <>
2023-10-17 16:33:25 +0000

    Add queries to the file and test

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -9,6 +9,18 @@ cursor = connection.cursor()
 # Query 1 - select all records from the "Artist" table
 cursor.execute('SELECT * FROM "Artist"')
 
+# Query 2 - select only the "Name" column from the "Artist" table
+cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only the albums with "ArtistId" #51 on the "Album" table
+cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
 # fetch the results (multiple)
 results = cursor.fetchall()
 
